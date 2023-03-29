4c97b4eef28fcf03e46799cea092cfd399bdf6a5
Zadorozhniy, Anton <anton.zadorozhniy@teradata.com>
2023-03-29 13:14:06 +0200

    Added a dummy model

--- model_definitions/mock-python/model_modules/training.py
+++ model_definitions/mock-python/model_modules/training.py
@@ -9,6 +9,6 @@ def train(context: ModelContext, **kwargs):
 
     palette=[(0x55,0x55,0x55), (0xff,0x99,0x99)]
     w = png.Writer(len(s[0]), len(s), palette=palette, bitdepth=1)
-    f = open('f"{context.artifact_output_path}/model.joblib"', 'wb')
+    f = open(f"{context.artifact_output_path}/model_chart.png", 'wb')
     w.write(f, s)
     f.close()
